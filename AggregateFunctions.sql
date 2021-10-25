59d7870c8786322bad45fe615e7e2c1175008141
snurliza <89376295+snurliza@users.noreply.github.com>
2021-10-25 13:38:12 +0800

    Update AggregateFunctions.sql

--- AggregateFunctions.sql
+++ AggregateFunctions.sql
@@ -76,7 +76,6 @@ FROM films
 -- Get the percentage of people who are no longer alive. Alias the result as percentage_dead. Remember to use 100.0 and not 100!
 -- get the count(deathdate) and multiply by 100.0
 -- then divide by count(*)
-
 SELECT COUNT(deathdate) * 100.0 / COUNT(*) AS percentage_dead
 FROM people
 
